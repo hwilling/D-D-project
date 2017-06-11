d0bcc8447d3f1884db2ad68f5fc839b65cc5534b
Hunter WIllingham <hwilling@ycp.edu>
2017-06-11 15:07:21 -0400

    created food object

--- D&D/src/Items/food.java
+++ D&D/src/Items/food.java
@@ -1,5 +1,33 @@
 package Items;
 
 public class food {
-
+	
+	private String name;
+	private int value;
+	private int weight;
+	
+	food(String name){
+		this.name = name;
+	}
+	//name of food
+	public String getName(){
+		return name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	//value of food
+	public int getValue(){
+		return value;
+	}
+	public void setValue(int value){
+		this.value = value;
+	}
+	//weight of food
+	public int getWeight(){
+		return weight;
+	}
+	public void setWeight(int weight){
+		this.weight = weight;
+	}
 }
