b29b78ea27b7a08d4f76ea08f3b6edd45372af3d
Hunter WIllingham <hwilling@ycp.edu>
2017-06-11 14:38:01 -0400

    added future layout for main function

--- D&D/src/Main/main.java
+++ D&D/src/Main/main.java
@@ -1,5 +1,17 @@
 package Main;
 
 public class main {
+	
+	static void main(String args[]){
+		//get players
+		
+		//assign DM
+		
+		//load global location informations such as cities and world map states
+		
+		//load characters from database or create new characters
+		
+		//pick or create boards to play on
 
+	}
 }
